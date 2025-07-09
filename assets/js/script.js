ceaac514c28572d000e4da4d7b616b8b5e15c8cc
Rebornian48 <VincentAndriK@users.noreply.github.com>
2025-07-09 10:13:10 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,130 +1,43 @@
+function getMonthName(monthIndex) {
+  const arrayBulan = [
+    "Januari", "Februari", "Maret", "April", "Mei", "Juni",
+    "Juli", "Agustus", "September", "Oktober", "November", "Desember"
+  ];
+  return arrayBulan[monthIndex];
+}
+
+function updateLinks(link) {
+  const diseases = [
+    "PMK", "LSD", "Rabies", "HPAI", "Anthraks",
+    "SE", "Jembrana", "ASF", "CSF", "Brucellosis", "Surra"
+  ];
+  diseases.forEach(disease => {
+    $("." + disease).attr("href", `./${disease}/${link}.html`);
+  });
+}
+
 function loadFirst() {
-    const today = new Date();
-    const year = today.getFullYear();
-    const month = String(today.getMonth()).padStart(2, "0");
-    const arrayBulan = [
-      "Januari",
-      "Februari",
-      "Maret",
-      "April",
-      "Mei",
-      "Juni",
-      "Juli",
-      "Agustus",
-      "September",
-      "Oktober",
-      "November",
-      "Desember",
-    ];
-    var namaBulan = arrayBulan[today.getMonth() - 1];
-    var link = `${year}${month}`;
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = String(today.getMonth()).padStart(2, "0");
+  const namaBulan = getMonthName(today.getMonth());
+  const link = `${year}${month}`;
 
-    
+  $(".bulanIni").text(`${namaBulan} ${year}`);
+  $("#report-month").val(`${year}-${month}`).attr("max", `${year}-${month}`);
+  updateLinks(link);
+}
 
-    $(".bulanIni").each(function () {
-      $(this).text(`${namaBulan} ${year}`);
-    });
-    $("#report-month").each(function () {
-      $(this).attr("value", `${year}-${month}`);
-    });
-    $("#report-month").each(function () {
-      $(this).attr("max", `${year}-${month}`);
-    });
-    $(".PMK").each(function () {
-      $(this).attr("href", "./PMK/" + link + ".html");
-    });
-    $(".LSD").each(function () {
-      $(this).attr("href", "./LSD/" + link + ".html");
-    });
-    $(".Rabies").each(function () {
-      $(this).attr("href", "./Rabies/" + link + ".html");
-    });
-    $(".HPAI").each(function () {
-      $(this).attr("href", "./HPAI/" + link + ".html");
-    });
-    $(".Anthraks").each(function () {
-      $(this).attr("href", "./Anthraks/" + link + ".html");
-    });
-    $(".SE").each(function () {
-      $(this).attr("href", "./SE/" + link + ".html");
-    });
-    $(".Jembrana").each(function () {
-      $(this).attr("href", "./Jembrana/" + link + ".html");
-    });
-    $(".ASF").each(function () {
-      $(this).attr("href", "./ASF/" + link + ".html");
-    });
-    $(".CSF").each(function () {
-      $(this).attr("href", "./CSF/" + link + ".html");
-    });
-    $(".Brucellosis").each(function () {
-      $(this).attr("href", "./Brucellosis/" + link + ".html");
-    });
-    $(".Surra").each(function () {
-      $(this).attr("href", "./Surra/" + link + ".html");
-    });
+function onReport() {
+  const reportMonth = $("#report-month").val();
+  if (!reportMonth) {
+    alert("Silakan pilih bulan terlebih dahulu. / Please select a month first.");
+    return;
   }
+  const [year, month] = reportMonth.split("-");
+  const namaBulan = getMonthName(parseInt(month, 10) - 1);
+  const link = `${year}${month}`;
 
-  function onReport() {
-    const reportMonth = document.getElementById("report-month").value;
-    if (reportMonth) {
-      const [year, month] = reportMonth.split("-");
-      var link = `${year}${month}`;
-      var bulan = month;
-      var tahun = year;
-    }
-    else {
-      alert("Silakan pilih bulan terlebih dahulu. / Please select a month first.");
-    }
-    const arrayBulan = [
-        "Januari",
-        "Februari",
-        "Maret",
-        "April",
-        "Mei",
-        "Juni",
-        "Juli",
-        "Agustus",
-        "September",
-        "Oktober",
-        "November",
-        "Desember",
-    ];
-    var namaBulan = arrayBulan[parseInt(bulan) - 1];
-     $(".bulanIni").each(function () {
-      $(this).text(`${namaBulan} ${tahun}`);
-    });
-    $(".PMK").each(function () {
-      $(this).attr("href", "./PMK/" + link + ".html");
-    });
-    $(".LSD").each(function () {
-      $(this).attr("href", "./LSD/" + link + ".html");
-    });
-    $(".Rabies").each(function () {
-      $(this).attr("href", "./Rabies/" + link + ".html");
-    });
-    $(".HPAI").each(function () {
-      $(this).attr("href", "./HPAI/" + link + ".html");
-    });
-    $(".Anthraks").each(function () {
-      $(this).attr("href", "./Anthraks/" + link + ".html");
-    });
-    $(".SE").each(function () {
-      $(this).attr("href", "./SE/" + link + ".html");
-    });
-    $(".Jembrana").each(function () {
-      $(this).attr("href", "./Jembrana/" + link + ".html");
-    });
-    $(".ASF").each(function () {
-      $(this).attr("href", "./ASF/" + link + ".html");
-    });
-    $(".CSF").each(function () {
-      $(this).attr("href", "./CSF/" + link + ".html");
-    });
-    $(".Brucellosis").each(function () {
-      $(this).attr("href", "./Brucellosis/" + link + ".html");
-    });
-    $(".Surra").each(function () {
-      $(this).attr("href", "./Surra/" + link + ".html");
-    });
-  }
+  $(".bulanIni").text(`${namaBulan} ${year}`);
+  updateLinks(link);
+}
